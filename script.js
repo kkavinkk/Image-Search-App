60b71104c9d096675f5dd1c3a968114c8e1756b2
kkavinkk <gk.kavin05@gmail.com>
2025-01-22 10:22:34 -0700

    last thing to do is to just register and apply the API key and fix the issue with the searchresults in css

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ let page = 1;
 
 async function searchImages(){
     inputData = inputEl.value;
-    const url = "https://api.unsplash.com/search.photos?page=${page}&query=${inputData}&client_id=${accessKey}"
+    const url = `https://api.unsplash.com/search.photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
-    const responce = await fetch(url)
-    const data = await responcse.json()
+    const response = await fetch(url)
+    const data = await response.json()
 
     const results = data.results
 
@@ -34,11 +34,11 @@ async function searchImages(){
 
         imageWrapper.appendChild(image)
         imageWrapper.appendChild(imageLink)
-        imageWrapper.appendChild(imageWrapper)
+        searchResults.appendChild(imageWrapper)
     });
 
     page++
-    if(page > 1){
+    if (page > 1){
         showMore.style.display = "block"
     }
 }
